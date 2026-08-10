b6cf7c2a2de3b3bf83c5c1d190f75ecdda299119
Vstalin Grady <vstalingrady@gmail.com>
2026-08-10 14:42:45 +0700

    ci: test and deploy Kolibri

---
+++ deploy/kolibri-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+
+repo=/home/ubuntu/gray/repo/course
+
+/usr/bin/node "$repo/server.js" --selftest >/dev/null
+/usr/bin/install -m 0644 "$repo/deploy/nginx.kolibri.alignment.id.conf" \
+  /etc/nginx/sites-enabled/kolibri.alignment.id.conf
+/usr/sbin/nginx -t
+/bin/systemctl restart kolibri-waitlist.service
+/bin/systemctl reload nginx
